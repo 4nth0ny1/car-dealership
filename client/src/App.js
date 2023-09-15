6ba79a72ccb367246126663afb71d27633915df9
4nth0ny1 <acatullo4@gmail.com>
2023-09-14 22:27:28 -0500

    added different backgrounds

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="bg-gradient-to-b from-cyan-500 to-blue-500 h-screen w-screen">
+        Hero
+      </div>
+      <div className="grainy-bg h-screen w-screen">
+        <h2 className="text-black text-3xl font-semibold">
+          Hello Grainy Background
+        </h2>
+      </div>
+      <div className="h-screen w-screen">Hero</div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+      10: "20px",
+      50: "50px",
+      100: "100px",
+      150: "150px",
+    },
   },
   plugins: [],
 };
